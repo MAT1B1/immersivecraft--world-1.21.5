f91fafc7480924f0baa180e51878e1094261ae20
Matibi <mathias.bagal@gmail.com>
2025-04-07 23:37:02 +0200

    New command to change season and correct number of tick after sleeping

--- src/main/java/com/matibi/immersivecraftworld/mixin/ServerWorldMixin.java
+++ src/main/java/com/matibi/immersivecraftworld/mixin/ServerWorldMixin.java
@@ -2,9 +2,7 @@ package com.matibi.immersivecraftworld.mixin;
 
 import com.matibi.immersivecraftworld.world.SeasonState;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.world.GameRules;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
